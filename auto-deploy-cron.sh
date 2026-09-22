03debb19629d6236f8c743ca5d024084a7744fe0
habibaaqandill <habibaaqandill@gmail.com>
2026-09-22 16:03:12 +0300

    UI update version v15.0

--- auto-deploy-cron.sh
+++ auto-deploy-cron.sh
@@ -3,14 +3,10 @@
 export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:$PATH
 
 cd /home/habiba/app-deployment
-
-# فحص التغييرات الجديدة على GitHub
 git fetch origin main >/dev/null 2>&1
 
 LOCAL_SHA=$(git rev-parse HEAD)
 REMOTE_SHA=$(git rev-parse origin/main)
-
-# إذا تم اكتشاف Push جديد
 if [ "$LOCAL_SHA" != "$REMOTE_SHA" ]; then
     echo "[$(date)] 🚀 New Git push detected! Deploying..." >> /home/habiba/app-deployment/cron-deploy.log
     git pull origin main >> /home/habiba/app-deployment/cron-deploy.log 2>&1
